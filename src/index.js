a24832c19bfbe6b1376bd2ca211ef9de13c8122f
Abishek Bardewa <49955388+abishekbardewa@users.noreply.github.com>
2021-03-02 14:28:50 +0530

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -12,9 +13,11 @@ import App from './App';
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
+			<PersistGate persistor={persistor}>
+				<React.StrictMode>
+					<App />
+				</React.StrictMode>
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,47 +1,10 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
-// import rootReducer from './root-reducer';
-
-// const middlewares = [logger];
-
-// const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-// export default store;
-
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-// convert object to string and store in localStorage
-function saveToLocalStorage(state) {
-	try {
-		const serialisedState = JSON.stringify(state);
-		localStorage.setItem('persistantState', serialisedState);
-	} catch (e) {
-		console.warn(e);
-	}
-}
 
-// load string from localStarage and convert into an Object
-// invalid output must be undefined
-function loadFromLocalStorage() {
-	try {
-		const serialisedState = localStorage.getItem('persistantState');
-		if (serialisedState === null) return undefined;
-		return JSON.parse(serialisedState);
-	} catch (e) {
-		console.warn(e);
-		return undefined;
-	}
-}
-
-// create our store from our rootReducers and use loadFromLocalStorage
-// to overwrite any values that we already have saved
-const store = createStore(rootReducer, loadFromLocalStorage(), applyMiddleware(...middlewares));
-
-// listen for store changes and use saveToLocalStorage to
-// save them to localStorage
-store.subscribe(() => saveToLocalStorage(store.getState()));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
